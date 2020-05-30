8f2109b7ee09832a9c6ffa8b0fd22ccacacd91c7
Barbi Semar Ryan Semar <semar@Barbis-MacBook-Air.local>
2020-05-30 18:45:56 -0500

    added a hashmap instead of arraylist

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
@@ -14,11 +15,11 @@ public class EventController {
 
     @GetMapping
     public String displayAllEvents(Model model) {
-        List<String> events = new ArrayList<>();
-        events.add("Code With Pride");
-        events.add("Strage Loop");
-        events.add("Apple WWDC");
-        events.add("SpringOne Platform");
+        HashMap<String,String> events = new HashMap<>();
+//        List<String> events = new ArrayList<>();
+        events.put("Code With Pride", "help underrepresented groups succeed in the tech industry");
+        events.put("Strange Loop", "Strange Loop is a multi-disciplinary conference that brings together the developers and thinkers building tomorrow's technology in fields such as emerging languages, alternative databases, concurrency, distributed systems, security, and the web.");
+        events.put("SpringOne Platform", "cloud native conference");
         model.addAttribute("events", events);
         return "events/index";
 
